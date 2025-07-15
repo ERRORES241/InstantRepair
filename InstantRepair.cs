d4b513f666720abd5f4710b16cbd4f792e6e5b17
Артем Мишко <Artem2281935@outlook.com>
2025-07-16 01:03:31 +0300

    Добавьте файлы проекта.

---
+++ InstantRepair.cs
@@ -0,0 +1,92 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTLD.Gear;
+using MelonLoader;
+
+namespace InstantRepair
+{
+    public class InstantRepairMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("InstantRepair mod initialized!");
+
+            uConsole.RegisterCommand("repair_all", new System.Action(RepairAllItems));
+            uConsole.RegisterCommand("repairall", new System.Action(RepairAllItems));
+
+            MelonLogger.Msg("Console commands registered: repair_all, repairall");
+        }
+
+        private void RepairAllItems()
+        {
+            try
+            {
+                Inventory inventory = GameManager.GetInventoryComponent();
+
+                if (inventory == null)
+                {
+                    MelonLogger.Warning("Cannot access inventory!");
+                    uConsole.Log("ERROR: Cannot access player inventory");
+                    return;
+                }
+
+                int repairedCount = 0;
+
+                Il2CppSystem.Collections.Generic.List<GearItemObject> items = inventory.m_Items;
+
+                for (int i = 0; i < items.Count; i++)
+                {
+                    GearItemObject gearItemObject = items[i];
+
+                    if (gearItemObject != null)
+                    {
+                        GearItem gearItem = gearItemObject.m_GearItem;
+
+                        if (gearItem != null && gearItem.CurrentHP < gearItem.GearItemData.m_MaxHP)
+                        {
+                            float oldCondition = gearItem.GetNormalizedCondition() * 100f;
+
+                            gearItem.CurrentHP = gearItem.GearItemData.m_MaxHP;
+
+                            gearItem.UpdateDamageShader();
+
+                            string itemName = gearItem.DisplayName;
+                            float newCondition = gearItem.GetNormalizedCondition() * 100f;
+
+                            MelonLogger.Msg($"Repaired: {itemName} ({oldCondition:F0}% -> {newCondition:F0}%)");
+
+                            repairedCount++;
+                        }
+                    }
+                }
+
+                if (repairedCount > 0)
+                {
+                    string message = $"Successfully repaired {repairedCount} item(s)!";
+                    uConsole.Log(message);
+
+                    HUDMessage.AddMessage(message, false, false);
+                }
+                else
+                {
+                    uConsole.Log("No items need repair");
+                    HUDMessage.AddMessage("No items need repair", false, false);
+                }
+            }
+            catch (System.Exception e)
+            {
+                MelonLogger.Error($"Error in RepairAllItems: {e.Message}");
+                uConsole.Log($"ERROR: Failed to repair items - {e.Message}");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(uConsole), "Start")]
+    public class ConsoleStartPatch
+    {
+        public static void Postfix()
+        {
+            MelonLogger.Msg("Developer Console loaded - InstantRepair commands available");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InstantRepair")]
+[assembly: AssemblyDescription("A mod for The Long Dark that adds console command to repair all items")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InstantRepair")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(InstantRepair.InstantRepairMod), "InstantRepair", "1.0.0", "ERRORESSS")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+[assembly: MelonColor(System.ConsoleColor.Cyan)]
+[assembly: MelonAuthorColor(System.ConsoleColor.Yellow)]
+
+[assembly: MelonOptionalDependencies("DeveloperConsole")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12345678-1234-1234-1234-123456789012")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
